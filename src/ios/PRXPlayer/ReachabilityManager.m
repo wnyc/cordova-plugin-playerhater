efad359b71a20c414671d0d6981f9f407af104a7
pouriamaleki <dev.maleki@gmail.com>
2015-04-13 11:42:43 +0430

    skipping reachability manager to avoid app crash when turning off offline media

--- src/ios/PRXPlayer/ReachabilityManager.m
+++ src/ios/PRXPlayer/ReachabilityManager.m
@@ -18,22 +18,22 @@
 
 - (id) init {
     self = [super init];
-    if (self) {
-        [[NSNotificationCenter defaultCenter] addObserver:self
-                                                selector:@selector(reachabilityDidChange:)
-                                                name:kReachabilityChangedNotification
-                                                object:nil];
-        self.previousReachability = -1;
-    }
+    // if (self) {
+    //     [[NSNotificationCenter defaultCenter] addObserver:self
+    //                                             selector:@selector(reachabilityDidChange:)
+    //                                             name:kReachabilityChangedNotification
+    //                                             object:nil];
+    //     self.previousReachability = -1;
+    // }
     return self;
 }
 
 - (void) reachabilityDidChange:(NSNotification *)notification {
     NSLog(@"reachabilityDidChange!");
-    
+
     CDVReachability * r = [notification object];
     NetworkStatus ns = [r currentReachabilityStatus];
-    
+
     if (self.delegate && [self.delegate respondsToSelector:@selector(reachabilityDidChangeFrom:to:)]) {
         [self.delegate reachabilityDidChangeFrom:self.previousReachability to:ns];
     }
